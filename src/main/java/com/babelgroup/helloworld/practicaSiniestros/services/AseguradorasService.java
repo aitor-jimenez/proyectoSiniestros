babdf891a9705cc865267721630f6f6fd1527ba0
Juan Antonio Montes <juanantonio.montes@babelgroup.com>
2025-03-24 12:42:48 +0100

    AseguradorasService
    Añadido AseguradorasService y AseguradorasConfig para crearlas

--- src/main/java/com/babelgroup/helloworld/practicaSiniestros/services/AseguradorasService.java
+++ src/main/java/com/babelgroup/helloworld/practicaSiniestros/services/AseguradorasService.java
@@ -1,4 +1,9 @@
 package com.babelgroup.helloworld.practicaSiniestros.services;
 
+import com.babelgroup.helloworld.practicaSiniestros.aseguradoras.Aseguradora;
+import com.babelgroup.helloworld.practicaSiniestros.siniestros.Siniestro;
+
 public interface AseguradorasService {
+    void elegirAseguradora();
+    void procesarSiniestro(Siniestro siniestro, Aseguradora aseguradora);
 }
---
+++ src/main/java/com/babelgroup/helloworld/practicaSiniestros/services/AseguradorasServiceImpl.java
@@ -0,0 +1,51 @@
+package com.babelgroup.helloworld.practicaSiniestros.services;
+
+import com.babelgroup.helloworld.practicaSiniestros.aseguradoras.Aseguradora;
+import com.babelgroup.helloworld.practicaSiniestros.siniestros.Siniestro;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+@Service
+public class AseguradorasServiceImpl implements AseguradorasService {
+
+    private List<Siniestro> siniestros;
+    private Map<String, Aseguradora> aseguradoras;
+
+    public AseguradorasServiceImpl(List<Siniestro> siniestros, Map<String, Aseguradora> aseguradoras) {
+        this.siniestros = siniestros;
+        this.aseguradoras = aseguradoras;
+    }
+
+    @Override
+    public void elegirAseguradora() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Elija una aseguradora (por nombre):");
+        for (String nombre : aseguradoras.keySet()) {
+            System.out.println(nombre);
+        }
+        String choice = scanner.nextLine();
+        Aseguradora aseguradoraElegida = aseguradoras.get(choice);
+        if (aseguradoraElegida == null) {
+            throw new IllegalArgumentException("Aseguradora no encontrada");
+        }
+
+        System.out.println("Elija un siniestro:");
+        for (int i = 0; i < siniestros.size(); i++) {
+            System.out.println((i + 1) + ". " + siniestros.get(i).getBienAfectado().getNombre());
+        }
+        int siniestroChoice = scanner.nextInt();
+        Siniestro siniestroElegido = siniestros.get(siniestroChoice - 1);
+
+        procesarSiniestro(siniestroElegido, aseguradoraElegida);
+    }
+
+
+    @Override
+    public void procesarSiniestro(Siniestro siniestro, Aseguradora aseguradora) {
+        double resultado = aseguradora.calcular(siniestro);
+        System.out.println("El resultado del cálculo es: " + resultado);
+    }
+}
