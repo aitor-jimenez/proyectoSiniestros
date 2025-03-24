c50146ad949b0a73ebf687b8156793066f8d90a6
Laura Reyes <laura.reyes@babelgroup.com>
2025-03-24 12:41:43 +0100

    añadido funciones a interfaz

--- src/main/java/com/babelgroup/helloworld/practicaSiniestros/services/SiniestrosService.java
+++ src/main/java/com/babelgroup/helloworld/practicaSiniestros/services/SiniestrosService.java
@@ -1,4 +1,11 @@
 package com.babelgroup.helloworld.practicaSiniestros.services;
 
+import com.babelgroup.helloworld.practicaSiniestros.siniestros.Siniestro;
+
+import java.util.List;
+
 public interface SiniestrosService {
+    void addSiniestro(Siniestro siniestro);
+    List<Siniestro> getSiniestros();
+    Siniestro solicitarDatosSiniestro();
 }
