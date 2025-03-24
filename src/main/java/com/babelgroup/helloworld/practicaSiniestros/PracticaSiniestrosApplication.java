692e5831d606db23893f0ec702cd7c48205ddbe4
Aitor Jiménez Segura <aitor.jimenez@babelgroup.com>
2025-03-24 09:41:19 +0100

    Primer commit

---
+++ src/main/java/com/babelgroup/helloworld/practicaSiniestros/PracticaSiniestrosApplication.java
@@ -0,0 +1,13 @@
+package com.babelgroup.helloworld.practicaSiniestros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaSiniestrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaSiniestrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/babelgroup/helloworld/practicaSiniestros/PracticaSiniestrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babelgroup.helloworld.practicaSiniestros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaSiniestrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
